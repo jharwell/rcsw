c36d4003b7e379cbc4c63e31d132c91fecc0929e
John Harwell <john.r.harwell@gmail.com>
2022-11-28 13:59:16 -0600

    Switch to MIT license

--- include/rcsw/common/exception.h
+++ include/rcsw/common/exception.h
@@ -1,9 +1,14 @@
 /**
  * \file exception.h
+ *
+ * \copyright 2009-2015 Francesco Nidito
+ * \copyright 2022 John Harwell All rights reserved.
+ *
  * \ingroup common
+ *
  * \brief Exception handling in C (woot woot!)
  *
- * Taken from online source and modified (original copyright below).
+ * SPDX-License-Identifier: MIT
  */
 
 #ifndef INCLUDE_RCSW_COMMON_EXCEPTION_H_
@@ -17,28 +22,6 @@
 /*******************************************************************************
  * Macros
  ******************************************************************************/
-/**
- * Copyright (C) 2009-2015 Francesco Nidito
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without __restrict__ion, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 /**
  * \brief Start of a try-catch block (just like in OOP languages)
  *
